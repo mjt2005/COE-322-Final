6fd098bd0b61c15aeb91f3f64576670e332b90bb
mjt2005 <mjt2005@login3.stampede3.tacc.utexas.edu>
2024-12-06 19:24:41 -0600

    plz work now

---
+++ functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <vector>
+#include "voter_class.h"
+#include "district.h"
+
+vector<Voter> generate_voters(int voter_num);
+vector<Voter> combine_like_voters(vector<Voter> &voters); 
+State ordered_districting(vector<Voter> &voters, int max_district_pop);
+State unordered_districting(vector<Voter> voters, int max_district_pop);
+void write_districts(State s);
+
+#endif
